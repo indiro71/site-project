d5360e2189fc3a890acb91f73880d680bc97dcdb
mickle <mickle@indiro.ru>
2023-02-13 20:23:15 +0300

    fix sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,6 +11,7 @@ interface SidebarProps {
 }
 
 export const Sidebar: FC<SidebarProps> = ({ className }) => {
+  const { t } = useTranslation();
   const [collapsed, setCollapsed] = useState<boolean>(false);
   const onToggle = () => {
     setCollapsed(prevState => !prevState);
@@ -17,9 +19,9 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
 
   return (
     <div data-testid="sidebar" className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-      {/* <Button data-testid="sidebar-toggle" onClick={onToggle}> */}
-      {/*  toggle */}
-      {/* </Button> */}
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        {t('toggle')}
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
