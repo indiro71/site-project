18785d9696b93c052635609412a75066e3d725b6
mickle <mickle@indiro.ru>
2023-02-26 00:45:32 +0300

    add login module

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,12 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { UserSchema } from '../types/user';
+import { PayloadAction } from '@reduxjs/toolkit/dist';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import { User, UserSchema } from '../types/user';
 
 const initialState: UserSchema = {};
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {}
+  reducers: {
+    setAuthData: (state, action: PayloadAction<User>) => {
+      state.authData = action.payload;
+    },
+    initAuthData: state => {
+      const user: User = JSON.parse(localStorage.getItem(USER_LOCALSTORAGE_KEY));
+      if (user) {
+        state.authData = user;
+      }
+    },
+    logout: state => {
+      state.authData = undefined;
+      localStorage.removeItem(USER_LOCALSTORAGE_KEY);
+    }
+  }
 });
 
 export const { actions: userActions } = userSlice;
