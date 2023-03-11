d6cb56bdecda348638640845e8dc43dcd4b302dc
mickle <mickle@indiro.ru>
2023-03-11 17:28:08 +0300

    validation

--- src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
+++ src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
@@ -1,15 +1,21 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { AxiosInstance } from 'axios';
 import { ThunkConfig } from 'app/providers/StoreProvider';
-import { Profile } from '../../types/profile';
+import { validateProfileData } from '../validateProfileData/validateProfileData';
+import { Profile, ValidateProfileError } from '../../types/profile';
 import { getProfileForm } from '../../selectors/getProfileForm/getProfileForm';
 
-export const updateProfileData = createAsyncThunk<Profile, void, ThunkConfig<string>>(
+export const updateProfileData = createAsyncThunk<Profile, void, ThunkConfig<ValidateProfileError[]>>(
   'profile/updateProfileData',
   async (_, thunkApi) => {
     const { extra, rejectWithValue, getState } = thunkApi;
 
     const formData = getProfileForm(getState());
+
+    const errors = validateProfileData(formData);
+
+    if (errors.length) {
+      return rejectWithValue(errors);
+    }
 
     try {
       const response = await extra.api.put<Profile>('/profile', formData);
@@ -17,7 +23,7 @@ export const updateProfileData = createAsyncThunk<Profile, void, ThunkConfig<str
       return response.data;
     } catch (e) {
       console.log(e);
-      return rejectWithValue('error');
+      return rejectWithValue([ValidateProfileError.SERVER_ERROR]);
     }
   }
 );
