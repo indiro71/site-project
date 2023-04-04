84acc89ea08d55ca440198e69897d8d0768457ce
mickle <mickle@indiro.ru>
2023-04-05 00:05:51 +0300

    fix eslint

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
-import { Portal } from 'shared/ui/Portal/Portal';
+import { Portal } from '../Portal/Portal';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
