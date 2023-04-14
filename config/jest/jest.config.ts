0b546726f26f31004ac92f964b47e50cbfde5de4
mickle <mickle@indiro.ru>
2023-04-14 21:41:29 +0300

    add jest reports

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -34,8 +34,19 @@ export default {
     __IS_DEV__: true,
     __API__: '',
     __PROJECT__: 'jest'
-  }
-
+  },
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: '<rootDir>/reports/unit',
+        filename: 'report.html',
+        openReport: true,
+        inlineSource: true
+      }
+    ]
+  ]
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
